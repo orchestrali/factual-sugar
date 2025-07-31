1b7c190881e68bcca98e1967793660e5eb5600b7
orchestrali <orchestrali@gmail.com>
2025-07-30 23:08:45 -0600

    Update test.js

--- src/test.js
+++ src/test.js
@@ -2,5 +2,7 @@ const fs = require("fs");
 const testdata = require("./data.json");
 
 module.exports = function teststuff() {
-  
+  fs.writeFile("data/testfile.json",JSON.stringify(testdata,null,2), err => {
+    console.log(err);
+  });
 }

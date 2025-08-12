bff516d1174c71fc76f2475db478b4db309c4896
orchestrali <orchestrali@gmail.com>
2025-08-12 15:42:33 +0100

    switch to fastify plugins???

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ const fastify = require("fastify")({
 });
 
 // Setup our static files
-fastify.register(require("fastify-static"), {
+fastify.register(require("@fastify/static"), {
   root: path.join(__dirname, "public"),
   prefix: "/" // optional: default '/'
 });
 
 // fastify-formbody lets us parse incoming forms
-fastify.register(require("fastify-formbody"));
+fastify.register(require("@fastify/formbody"));
 
 // point-of-view is a templating manager for fastify
 fastify.register(require("point-of-view"), {

e968e08a9a160428d123bd4b185a4a0074fb0e8b
orchestrali <orchestrali@gmail.com>
2025-10-03 17:33:20 +0100

    Create router.js

---
+++ src/mongodb/router.js
@@ -0,0 +1,12 @@
+const connectsearch = require("./startconnect.js");
+const models = ["method", "performance", "oldPerformance", "name", "bbperformance", "unmethod", "variation", "call", "cycle", "leadhead", "methodab", "doublescall", "word", "bbtext"];
+
+module.exports = async function router(model, req) {
+  if (models.includes(model)) {
+    let o = {model: model, query: req};
+    const results = await connectsearch(o);
+    return results;
+  } else {
+    return {error: "no such collection"};
+  }
+}

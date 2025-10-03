f5a4fb78c1d105d74a947ff267c4eb8c57d09159
orchestrali <orchestrali@gmail.com>
2025-10-03 18:56:35 +0100

    Update router.js

--- src/mongodb/router.js
+++ src/mongodb/router.js
@@ -5,6 +5,7 @@ module.exports = async function router(model, req) {
   if (models.includes(model)) {
     let o = {model: model+"s", query: JSON.parse(req)};
     const results = await connectsearch(o);
+    console.log("num results: "+results.length);
     return results;
   } else {
     return {error: "no such collection"};

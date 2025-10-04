2f300bcdc784ee783837f89be9291f096452d394
orchestrali <orchestrali@gmail.com>
2025-10-04 01:02:22 +0100

    add object???

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ fastify.get("/methods", function(request, reply) {
   reply.view("/src/pages/methods.html");
 });
 
-fastify.post("/find/:model", async function(req, reply) {
+fastify.post("/find/:model", {}, async function(req, reply) {
   console.log(req.body);
   //const results = await mongorouter(req.params.model, req.body);
   //console.log("results acquired");

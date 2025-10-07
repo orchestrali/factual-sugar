5299d7604cf8594634e9e43424b0c1148b6979fb
orchestrali <orchestrali@gmail.com>
2025-10-07 18:31:28 +0100

    Update buildquery2.js

--- src/mongodb/buildquery2.js
+++ src/mongodb/buildquery2.js
@@ -1,22 +1,27 @@
 const numbers = ["stage", "leadLength", "numHunts", "huntBells", "huntPath", "stationaryBells", "numWorking", "pbOrder", "leadsInCourse", "ccNum"];
 const booleans = ["classification.little", "classification.differential", "classification.plain", "classification.trebleDodging", "leadtruth", "coursetruth"];
-const others = ["title", "oldtitle", "name", "class", "leadHead", "leadHeadCode", "fchGroups", "symmetry", "pn", "refs", "notes"];
+const others = ["title", "oldtitle", "name", "class", "leadHead", "leadHeadCode", "fchGroups", "symmetry", "pn", "pnFull", "refs", "notes"];
 
 //mongodb query sent via query params
 //is there a clever way to do this with models
 module.exports = function buildquery(q) {
   let query = {};
   for (let key in q) {
+    let val = q[key];
+    if ((val.startsWith("{") && val.endsWith("}")) || (val.startsWith("[") && val.endsWith("]"))) {
+      val = JSON.parse(val);
+    }
     if (numbers.includes(key)) {
-      if (typeof q[key] === "string") {
-        query[key] = Number(q[key]);
-      } else if (Array.isArray(q[key])) {
-        query[key] = arrstringstonums(q[key]);
+      if (typeof val === "string") {
+        query[key] = Number(val);
+      } else if (Array.isArray(val)) {
+        query[key] = val;
+          //arrstringstonums(q[key]); JSON.parse should take care of it???
       }
     } else if (booleans.includes(key)) {
-      query[key] = q[key] === "true";
+      query[key] = val === "true";
     } else if (others.includes(key)) {
-      query[key] = q[key];
+      query[key] = val;
     }
   }
   return query;

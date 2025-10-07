9cd3e25a0b0201d5f4f7abbb57326ae3b9379eb5
orchestrali <orchestrali@gmail.com>
2025-10-07 12:23:26 +0100

    Create find.js

---
+++ src/mongodb/find.js
@@ -0,0 +1,9 @@
+
+
+module.exports = function find(db, mod, query) {
+  const coll = db.collection(mod);
+
+  const cursor = coll.find(query);
+  const array = await cursor.toArray();
+  return array;
+}

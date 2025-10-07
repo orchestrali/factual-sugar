864e88099909fa619bc51d036a9bce57831e223c
orchestrali <orchestrali@gmail.com>
2025-10-07 02:11:24 +0100

    turn off logs

--- src/mongodb/startconnect.js
+++ src/mongodb/startconnect.js
@@ -4,17 +4,17 @@ const find = require("./findfields.js");
 const uri = "mongodb+srv://"+process.env.USER+":"+process.env.PASS+"@cluster0.wompx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 module.exports = async function runGetStarted(o) {
-  console.log("attempting to connect");
+  //console.log("attempting to connect");
   const client = new MongoClient(uri);
 
   try {
     const database = client.db('bellringing');
 
     if (o) {
-      console.log("searching");
-      console.log(o.query);
+      //console.log("searching");
+      //console.log(o.query);
       const results = await find(database, o.model, o.query);
-      console.log(results.length);
+      //console.log(results.length);
       return results;
     }
     /*

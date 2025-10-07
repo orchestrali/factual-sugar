08f222c525719157b149bd1bd9df1f441cacb70b
orchestrali <orchestrali@gmail.com>
2025-10-07 14:43:45 +0100

    Create getrouter.js

---
+++ src/mongodb/getrouter.js
@@ -0,0 +1,13 @@
+const connectsearch = require("./startconnect.js");
+const models = ["method", "performance", "oldPerformance", "name", "bbperformance", "unmethod", "variation", "call", "cycle", "leadhead", "methodab", "doublescall", "word", "bbtext"];
+
+module.exports = async function router(model, req) {
+  if (models.includes(model)) {
+    let o = {model: model+"s", query: req || {}};
+    const results = await connectsearch("./findfields.js", o);
+    console.log("num results: "+results.length);
+    return results;
+  } else {
+    return {error: "no such collection"};
+  }
+}

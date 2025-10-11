32ff5e3ab82a84f2165a60f8d18fefd3ec854ba0
orchestrali <orchestrali@gmail.com>
2025-10-11 22:20:36 +0100

    fix collection views

--- public/scriptmethods.js
+++ public/scriptmethods.js
@@ -804,8 +804,10 @@ function cancelemptycoll() {
 
 function viewcoll(cid) {
   currentscreen = "collectionpanel";
+  collview = "list";
+  $("#toggleview").text("View as grids");
   $("#gridcontainer").hide();
-  $("#collectionpanel table").show();
+  $("#editcollection,#collsort,#collectionpanel table").show();
   let all = cid === "all-my-methods";
   let cmethods = all ? mymethods : mymethods.filter(m => m.collections.includes(cid));
   currentcollection = all ? allmymethods : mycollections.find(c => c.id === cid);
@@ -1324,7 +1326,7 @@ function drawmethod(m, gridtype, rowarr, target) {
   let width = 40 + 16*stage;
   let height = 20; //target ? 40 : 20;
   height += 20*rowarr.length;
-  console.log("height: "+height);
+  //console.log("height: "+height);
   
     //gridtype === "grid" ? m.leadLength*20 : m.leadLength*m.leadsInCourse*20;
   let grid = svg.svg($(topparent), null, null, width, height, {class: "grid", xmlns: "http://www.w3.org/2000/svg", "xmlns:xlink": "http://www.w3.org/1999/xlink"});
@@ -1343,9 +1345,9 @@ function drawmethod(m, gridtype, rowarr, target) {
     //liney += 20;
     let x = 16*stage+4;
     let mtitle = svg.text(numgroup, x, 9, m.title, {transform: "rotate(90 "+x.toString()+" 9)", style: "font-size: 12px; font-weight: bold;"});
-    let length = Math.ceil($(mtitle).width());
+    let length = 7*m.title.length;
     console.log(length);
-    if (length + 12 > height) $(grid).attr("height", length+12);
+    if (length + 9 > height) $(grid).attr("height", length+12);
   }
   let pbs = [];
   
